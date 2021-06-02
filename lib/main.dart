ef5ef4b44ade2cf6e6dbc5816f705b67e1f38a7c
Victor Hugo <vicktor_1212@hotmail.com>
2021-06-01 19:45:34 -0500

    add inputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:example_flutter/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Test',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Spanish, no country code
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/src/screens/components/pages/inputs.page.dart
+++ lib/src/screens/components/pages/inputs.page.dart
@@ -6,52 +6,154 @@ class InputsPage extends StatefulWidget {
 }
 
 class _InputsPageState extends State<InputsPage> {
-
-  String _name;
+  String _name = '';
+  String _email = '';
+  String _date = '';
+  String _optSelected = 'Fly';
+  TextEditingController _inputDateController = TextEditingController();
+  List<String> _powersList = ['Fly', 'Rayos X', 'Fuerza'];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(title: Text('Inputs')),
-        body: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-          children: <Widget>[
-            _createInput(),
-            Devider(),
-            _createPerson(),
-          ],
-        )
-      ),
+          appBar: AppBar(title: Text('Inputs')),
+          body: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+            children: <Widget>[
+              _createInput(),
+              Divider(),
+              _createEmail(),
+              Divider(),
+              _createPassword(),
+              Divider(),
+              _createDate(context),
+              Divider(),
+              _createDropdown(),
+              Divider(),
+              _createPerson(),
+            ],
+          )),
     );
   }
 
   Widget _createInput() {
     return TextField(
-      autofocus: false,
-      textCapitalization: TextCapitalization.sentences,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        counter: Text('${_name.length} Letters'),
-        hintText: 'Name',
-        labelText: 'Name',
-        helperText: 'Only name',
-        suffixIcon: Icon(Icons.person),
-        icon: Icon(Icons.account_circle)
-      ),
-      onChanged: (name) {
-        setState(() {
-          _name = name;
+        autofocus: false,
+        textCapitalization: TextCapitalization.sentences,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            counter: Text('${_name.length} Letters'),
+            hintText: 'Name',
+            labelText: 'Name',
+            helperText: 'Only name',
+            suffixIcon: Icon(Icons.person),
+            icon: Icon(Icons.account_circle)),
+        onChanged: (name) {
+          setState(() {
+            _name = name;
+          });
         });
-      }
-    );
   }
 
   Widget _createPerson() {
-    return ListTitle(
-      title: Text('The name is: $_name')
+    return ListTile(
+      title: Text('The name is: $_name'),
+      subtitle: Text('The email is: $_email'),
+      trailing: Text(_optSelected),
     );
+  }
+
+  Widget _createEmail() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Email',
+            labelText: 'Email',
+            suffixIcon: Icon(Icons.alternate_email),
+            icon: Icon(Icons.email)),
+        onChanged: (data) {
+          setState(() {
+            _email = data;
+          });
+        });
+  }
+
+  Widget _createPassword() {
+    return TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Password',
+            labelText: 'Password',
+            suffixIcon: Icon(Icons.lock_open),
+            icon: Icon(Icons.lock)),
+        onChanged: (data) {});
+  }
+
+  Widget _createDate(BuildContext context) {
+    return TextField(
+        controller: _inputDateController,
+        enableInteractiveSelection: false,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+            hintText: 'Date',
+            labelText: 'Date',
+            suffixIcon: Icon(Icons.calendar_today),
+            icon: Icon(Icons.calendar_today)),
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+          _selectDate(context);
+        });
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2019),
+        lastDate: new DateTime(2025),
+        locale: Locale('es', 'ES'));
+
+    if (picked != null) {
+      setState(() {
+        _date = picked.toString();
+        _inputDateController.text = _date;
+      });
+    }
+  }
+
+  List<DropdownMenuItem<String>> getOptionsDropdown() {
+    List<DropdownMenuItem<String>> list = [];
+    _powersList.forEach((p) {
+      list.add(DropdownMenuItem(
+        child: Text(p),
+        value: p,
+      ));
+    });
+    return list;
+  }
+
+  Widget _createDropdown() {
+    return Row(children: [
+      Icon(Icons.select_all),
+      SizedBox(
+        width: 30.0,
+      ),
+      Expanded(
+        child: DropdownButton(
+          value: _optSelected,
+          items: getOptionsDropdown(),
+          onChanged: (opt) {
+            _optSelected = opt.toString();
+          },
+        ),
+      )
+    ]);
   }
 }
