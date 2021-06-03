fb9aae921a414e2522e24a6103068014cd72670e
Victor Hugo Tellez IZquierdo <vicktor_1212@hotmail.com>
2021-06-02 22:25:48 -0500

    infinite scroll

--- lib/src/screens/components/pages/list_page.dart
+++ lib/src/screens/components/pages/list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ListPage extends StatefulWidget {
@@ -9,6 +11,7 @@ class _ListPageState extends State<ListPage> {
 
   List<int> _listInt = [];
   int _lastItem = 0;
+  bool _isLoading = false;
   ScrollController _scrollController = new ScrollController();
 
   @override
@@ -17,16 +20,28 @@ class _ListPageState extends State<ListPage> {
     _addMore();
     _scrollController.addListener(() {
       if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        _addMore();
+        //_addMore();
+        _fetchData();
       }
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('List view'),),
-      body: _createList()
+      body: Stack(
+        children: <Widget>[
+          _createList(),
+          _createLoading()
+        ],
+      )
     );
   }
 
@@ -45,11 +60,50 @@ class _ListPageState extends State<ListPage> {
     );
   }
 
+  Widget _createLoading() {
+    //return _isLoading ? CircularProgressIndicator() : Container();
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(height: 180.0,)
+            ],
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
+
   void _addMore() {
     for (var i = 0; i < 10; i++) {
       _lastItem++;
       _listInt.add(_lastItem);
     }
     setState(() {});
+  }
+
+  Future _fetchData() async {
+    _isLoading = true;
+    setState(() {});
+
+    final duration = Duration(seconds: 2);
+    return new Timer(duration, httpResponse);
+  }
+
+  void httpResponse () {
+    _isLoading = false;
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      curve: Curves.fastOutSlowIn,
+      duration: Duration(microseconds: 150)
+    );
+    _addMore();
   }
 }
