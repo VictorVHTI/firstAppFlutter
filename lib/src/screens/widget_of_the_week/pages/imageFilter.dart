1002b0e67618adefaf1464d6a0dfee3863dbf1be
Victor Hugo Tellez Izquierdo <tellez.vhi@gmail.com>
2021-06-18 19:28:25 -0500

    transform

--- lib/src/screens/widget_of_the_week/pages/imageFilter.dart
+++ lib/src/screens/widget_of_the_week/pages/imageFilter.dart
@@ -1,10 +1,42 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MyImageFilter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        Center(
+          child: Image.network(
+              'https://www.androiddeveloper.co.in/blog/wp-content/uploads/2019/12/How-Google-Flutter-Became-The-Best-SDK-for-Mobile-App-Development.png'),
+        ),
+        Positioned(
+          top: 330,
+          bottom: 350,
+          left: 20,
+          right: 20,
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 5,
+                sigmaY: 5,
+              ),
+              child: Container(
+                  //color: Colors.red.withOpacity(0.4),
+                  ),
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'No Blur',
+            style: TextStyle(
+                fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        )
+      ],
+    ));
   }
 }
--- lib/src/screens/widget_of_the_week/pages/transform.dart
+++ lib/src/screens/widget_of_the_week/pages/transform.dart
@@ -1,13 +1,64 @@
 import 'package:flutter/material.dart';
 
-class MytTansform extends StatelessWidget {
+class MytTansform extends StatefulWidget {
+  @override
+  _MytTansformState createState() => _MytTansformState();
+}
+
+class _MytTansformState extends State<MytTansform>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> scaleAnimation;
+  late Animation<double> translateAnimation;
+  @override
+  void initState() {
+    animationController =
+        new AnimationController(duration: Duration(seconds: 1), vsync: this);
+    scaleAnimation =
+        Tween<double>(begin: 1, end: 2).animate(animationController);
+    translateAnimation = Tween<double>(begin: 0, end: 50).animate(
+        CurvedAnimation(parent: animationController, curve: Curves.bounceOut));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _Square(),
-      ),
-    );
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedBuilder(
+            animation: animationController,
+            builder: (BuildContext context, _) {
+              return Transform.scale(
+                  scale: scaleAnimation.value,
+                  child: Transform.translate(
+                      offset: (Offset(translateAnimation.value, 0)),
+                      child: Transform(
+                          transform: Matrix4.skewX(0), child: _Square())));
+            }),
+        SizedBox(
+          height: 100,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                animationController.reset();
+              },
+              child: Icon(Icons.restore),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                animationController.forward();
+              },
+              child: Icon(Icons.play_arrow_outlined),
+            )
+          ],
+        )
+      ],
+    ));
   }
 }
 
